0428dc698dacd3b339425e3c991499833c5581e4
braph <braph93@gmx.de>
2017-03-19 16:13:31 +0100

    compat fix

--- lib/ektoplayer/compat.rb
+++ lib/ektoplayer/compat.rb
@@ -1,19 +1,23 @@
 unless Array.public_method_defined? :sum
-   def Array.sum
-      result = 0
-      self.each { |i| result += 1 }
-      result
+   class Array
+      def sum
+         result = 0
+         self.each { |i| result += 1 }
+         result
+      end
    end
 end
 
 unless Integer.public_method_defined? :clamp
-   def Integer.clamp(min, max)
-      if self < min
-         min
-      elsif self > max
-         max
-      else
-         self
+   class Integer
+         def clamp(min, max)
+         if self < min
+            min
+         elsif self > max
+            max
+         else
+            self
+         end
       end
    end
 end
